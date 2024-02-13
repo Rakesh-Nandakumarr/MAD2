3cd84fc3c5fd8aaf4c242ea49f95a5263289ac1f
Rakesh4584 <Rakeshnandakumar2005@gmail.com>
2024-02-13 20:03:47 +0000

    i am so absent minded

--- resources/views/admin/product_category/index.blade.php
+++ resources/views/admin/product_category/index.blade.php
@@ -24,7 +24,7 @@
                 <div class="sm:flex-auto">
                     <h1 class="text-base font-semibold leading-6 text-gray-900">Product Categories</h1>
                     <p class="mt-2 text-sm text-gray-700">
-                        A list of all the product categories in your account including their name, slug and actions.
+                        A list of all the product categories in your account including their name, link and actions.
                     </p>
                 </div>
                 <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
