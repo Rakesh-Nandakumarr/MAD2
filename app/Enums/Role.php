e84bbf206f2b9910f2345e6b820acb68ff6f6d58
Rakesh4584 <Rakeshnandakumar2005@gmail.com>
2024-02-15 07:10:03 +0000

    role update

--- app/Enums/Role.php
+++ app/Enums/Role.php
@@ -4,7 +4,7 @@ namespace App\Enums;
 enum Role: int
 {
     case SuperAdministrator = 1;
-    case KitchenManager = 2;
+    case InventoryManager = 2;
     case MarketingManager = 3;
     case SalesManager = 4;
     case FinanceManager = 5;
